8e6cb7dc5873d204e9e429231de66785c2102398
Grant Dennison <grant.dennison@gmail.com>
2022-06-14 08:13:42 -0400

    Flesh out type prelude generation

--- src/code-gen/ts/block/generate-block-ts.ts
+++ src/code-gen/ts/block/generate-block-ts.ts
@@ -2,9 +2,10 @@ import type { Block } from "../../../tree/block";
 import { nodeError, TreeNode } from "../../../tree/tree-node";
 import { contextType, GeneratorState } from "../../generator-state";
 import { genIndent, makeIndent } from "../../indent-string";
-import { fromComplicated, fromToken, fromTokenRange, GeneratedSnippet, GeneratedSnippets, GeneratorFixture, IndependentCodeGeneratorFunc } from "../../generator";
+import { fromComplicated, fromNode, fromToken, fromTokenRange, GeneratedSnippet, GeneratedSnippets, GeneratorFixture, IndependentCodeGeneratorFunc } from "../../generator";
 import { checkAndGenerateTypeInstanceTs, generateTypeInstanceTs } from "../generate-type-instance-ts";
 import assert from "assert";
+import type { Call, TypeCall } from "../../../tree";
 
 export function tryGenerateBlockTs(node: TreeNode, state: GeneratorState, fixture: GeneratorFixture): void | GeneratedSnippets {
     if (node.type === "block") {
@@ -18,29 +19,39 @@ export function generateBlockTs(block: Block, state: GeneratorState, fixture: Ge
     }
     
     let preludeTypeSnippetsDone = false
-    const typeParameterSpecs: GeneratedSnippets = []
     const parameterSpecs: GeneratedSnippets = []
     let returnTypeSpec: GeneratedSnippets | null = null
-    const preludeTypeSnippets: GeneratedSnippets = []
+    let preludeTypeInfo: PreludeTypeInfo = {
+        typeParameters: [],
+        typeSnippets: [],
+    }
     const imperativeIdeas: Block["ideas"] = []
     for (const idea of block.ideas) {
-        const tps = getTypeParameterSpec(idea, state, fixture);
+        const tps = getTypeParameterSpec(idea, state, fixture, preludeTypeInfo);
         if (tps !== null) {
-            if (typeParameterSpecs.length > 0) {
-                typeParameterSpecs.push(fromTokenRange(block, ", "))
+            let inlineSnippets = preludeTypeInfo.typeParameters.length > 0
+                ? [fromTokenRange(block, ", "), tps.inlineParamSnippet]
+                : tps.inlineParamSnippet
+            preludeTypeInfo = {
+                typeParameters: [...preludeTypeInfo.typeParameters, {
+                    name: tps.paramName,
+                    inlineSnippet: inlineSnippets,
+                }],
+                typeSnippets: [...preludeTypeInfo.typeSnippets, tps.blockSnippet],
             }
-            typeParameterSpecs.push(tps.inlineParamSnippet)
-            preludeTypeSnippets.push(tps.blockSnippet)
             continue
         }
         if (!preludeTypeSnippetsDone && idea.type === "type-assignment") {
             const gen = fixture.generate(idea, state)
-            preludeTypeSnippets.push(gen)
+            preludeTypeInfo = {
+                typeParameters: preludeTypeInfo.typeParameters,
+                typeSnippets: [...preludeTypeInfo.typeSnippets, gen],
+            }
             continue
         }
         preludeTypeSnippetsDone = true
 
-        const ps = getParameterSpec(idea, state, fixture)
+        const ps = getParameterSpec(idea, state, fixture, preludeTypeInfo)
         if (ps !== null) {
             if (parameterSpecs.length > 0) {
                 parameterSpecs.push(fromTokenRange(block, ", "))
@@ -49,7 +60,7 @@ export function generateBlockTs(block: Block, state: GeneratorState, fixture: Ge
             continue
         }
 
-        const rts = getReturnTypeSpec(idea, state, fixture)
+        const rts = getReturnTypeSpec(idea, state, fixture, preludeTypeInfo)
         if (rts !== null) {
             if (returnTypeSpec === null) {
                 returnTypeSpec = rts
@@ -76,11 +87,12 @@ export function generateBlockTs(block: Block, state: GeneratorState, fixture: Ge
     }
 
     const definition = fromComplicated(block, [
-        ...(typeParameterSpecs.length === 0 ? [] : ["<", typeParameterSpecs, ">"]),
+        ...(preludeTypeInfo.typeParameters.length === 0
+            ? [] : ["<", preludeTypeInfo.typeParameters.map(tp => tp.inlineSnippet), ">"]),
         "(", parameterSpecs, ")", returnTypeSpec ?? [], " => {\n",
-        ...preludeTypeSnippets.map(s => [makeIndent(state.indentLevel + 1), s, "\n"]).flat(),
+        ...preludeTypeInfo.typeSnippets.map(s => [makeIndent(state.indentLevel + 1), s, "\n"]).flat(),
         linesTs,
-        makeIndent(state.indentLevel), "}"
+        makeIndent(state.indentLevel), "}",
     ])
 
     if (state.context === contextType.looseExpression) {
@@ -89,11 +101,60 @@ export function generateBlockTs(block: Block, state: GeneratorState, fixture: Ge
     return definition
 }
 
+type PreludeTypeInfo = {
+    typeParameters: readonly { readonly name: string, readonly inlineSnippet: GeneratedSnippets }[]
+    typeSnippets: readonly GeneratedSnippets[]
+}
+
+function generateTypeParams(node: TreeNode, preludeTypeInfo: PreludeTypeInfo): {
+    params: GeneratedSnippets,
+    args: GeneratedSnippets
+} {
+    if (preludeTypeInfo.typeParameters.length === 0) {
+        return { params: fromNode(node, ""), args: fromNode(node, "") }
+    }
+    return {
+        params: fromComplicated(node, ["<", preludeTypeInfo.typeParameters.map(tp => tp.inlineSnippet), ">"]),
+        args: fromComplicated(node, [
+            "<", fromNode(node, preludeTypeInfo.typeParameters.map(tp => tp.name).join(", ")), ">"
+        ]),
+    }
+}
+
+function generatePreStatementAndTypeForParam(
+    givenCallNode: Call | TypeCall,
+    state: GeneratorState,
+    fixture: GeneratorFixture,
+    preludeTypeInfo: PreludeTypeInfo,
+    name?: string,
+) {
+    const typeNode = givenCallNode.type === 'call' ? givenCallNode.typeArgs[0] : givenCallNode.args[0]
+    const intermediateName = (name ? `_${name}` : state.getUniqueVariableName()) + "_TypePackage"
+    const intermediateTypeParams = generateTypeParams(givenCallNode, preludeTypeInfo)
+    const extendedTypeSnippet = checkAndGenerateTypeInstanceTs(typeNode, state, fixture)
+
+    state.addPreStatementGenerator((s, f) => {
+        const indent = makeIndent(s.indentLevel)
+        const indent2 = makeIndent(s.indentLevel + 1)
+        return fromComplicated(givenCallNode, [
+            `class ${intermediateName}`, intermediateTypeParams.params, ` { f() {\n`,
+            preludeTypeInfo.typeSnippets.map(snippet => fromComplicated(givenCallNode, [indent2, snippet, "\n"])),
+            indent2, `return undefined as unknown as `, extendedTypeSnippet, "\n",
+            indent, `} }`
+        ])
+    })
+
+    return fromComplicated(typeNode, [
+        'ReturnType<', intermediateName, intermediateTypeParams.args, `["f"]>`
+    ])
+}
+
 function getTypeParameterSpec(
     node: TreeNode,
     state: GeneratorState,
-    fixture: GeneratorFixture
-): { inlineParamSnippet: GeneratedSnippets, blockSnippet: GeneratedSnippets } | null {
+    fixture: GeneratorFixture,
+    preludeTypeInfo: PreludeTypeInfo,
+): { paramName: string, inlineParamSnippet: GeneratedSnippets, blockSnippet: GeneratedSnippets } | null {
     if (node.type !== "type-assignment") {
         return null
     }
@@ -111,8 +172,28 @@ function getTypeParameterSpec(
     const paramName = `_${node.variable.text}_Param`
     const inlineParamSnippet: GeneratedSnippets[] = [fromToken(node.variable, paramName)]
     if (node.call.args.length > 0) {
-        const extendedTypeSnippet = checkAndGenerateTypeInstanceTs(node.call.args[0], state, fixture)
-        inlineParamSnippet.push(fromComplicated(node.call.args[0], [' extends ', extendedTypeSnippet]))
+        // const intermediateName = `_${node.variable.text}_SuperPackage`
+        // const intermediateTypeParams = generateTypeParams(node, preludeTypeInfo)
+        // const extendedTypeSnippet = checkAndGenerateTypeInstanceTs(node.call.args[0], state, fixture)
+
+        // state.addPreStatementGenerator((s, f) => {
+        //     const indent = makeIndent(s.indentLevel)
+        //     const indent2 = makeIndent(s.indentLevel + 1)
+        //     return fromComplicated(node, [
+        //         `class ${intermediateName}`, intermediateTypeParams.params, ` { f() {\n`,
+        //         preludeTypeInfo.typeSnippets.map(snippet => fromComplicated(node, [indent2, snippet, "\n"])),
+        //         indent2, `return undefined as unknown as `, extendedTypeSnippet, "\n",
+        //         indent, `} }`
+        //     ])
+        // })
+
+        // inlineParamSnippet.push(fromComplicated(node.call.args[0], [
+        //     ' extends ReturnType<', intermediateName, intermediateTypeParams.args, `["f"]>`
+        // ]))
+
+        inlineParamSnippet.push(fromComplicated(node.call.args[0], [
+            ' extends ', generatePreStatementAndTypeForParam(node.call, state, fixture, preludeTypeInfo, node.variable.text)
+        ]))
 
         if (node.call.args.length > 1) {
             const defaultTypeSnippet = checkAndGenerateTypeInstanceTs(node.call.args[1], state, fixture)
@@ -121,12 +202,18 @@ function getTypeParameterSpec(
     }
 
     return {
+        paramName: paramName,
         inlineParamSnippet: inlineParamSnippet,
         blockSnippet: fromComplicated(node, ['const ', node.variable.text, ' = undefined as unknown as ', paramName])
     }
 }
 
-function getParameterSpec(node: TreeNode, state: GeneratorState, fixture: GeneratorFixture): GeneratedSnippets | null {
+function getParameterSpec(
+    node: TreeNode,
+    state: GeneratorState,
+    fixture: GeneratorFixture,
+    preludeTypeInfo: PreludeTypeInfo,
+): GeneratedSnippets | null {
     // TODO: Also support `given` call without assignment (ignored parameter)
     // TODO: Decide and implement support for non-constant assignments
     if (node.type !== "assignment") {
@@ -151,7 +238,10 @@ function getParameterSpec(node: TreeNode, state: GeneratorState, fixture: Genera
         state.addError(nodeError(arg, `given cannot take more than 1 argument`))
     }
 
-    const typeSnippet = node.call.typeArgs.length === 0 ? [] : [": ", generateTypeInstanceTs(node.call.typeArgs[0], state, fixture)]
+    const typeInstanceSnippet = preludeTypeInfo.typeSnippets.length === 0
+        ? generateTypeInstanceTs(node.call.typeArgs[0], state, fixture)
+        : generatePreStatementAndTypeForParam(node.call, state, fixture, preludeTypeInfo, node.variable.token.text)
+    const typeSnippet = node.call.typeArgs.length === 0 ? [] : [": ", typeInstanceSnippet]
     const defaultValueSnippet = node.call.args.length === 0 ? [] : [" = ", fixture.generate(node.call.args[0], state.makeChild({ context: contextType.isolatedExpression }))]
     return fromComplicated(node, [fromToken(node.variable.token), ...typeSnippet, ...defaultValueSnippet])
 }
@@ -159,7 +249,8 @@ function getParameterSpec(node: TreeNode, state: GeneratorState, fixture: Genera
 function getReturnTypeSpec(
     node: TreeNode,
     state: GeneratorState,
-    fixture: GeneratorFixture
+    fixture: GeneratorFixture,
+    preludeTypeInfo: PreludeTypeInfo,
 ): GeneratedSnippets | null {
     if (node.type !== "type-call") {
         return null
@@ -180,7 +271,10 @@ function getReturnTypeSpec(
         state.addError(nodeError(arg, `type return cannot take more than 1 argument`))
     }
 
-    return fromComplicated(node, [": ", checkAndGenerateTypeInstanceTs(node.args[0], state, fixture)])
+    const typeInstanceSnippet = preludeTypeInfo.typeSnippets.length === 0
+        ? checkAndGenerateTypeInstanceTs(node.args[0], state, fixture)
+        : generatePreStatementAndTypeForParam(node, state, fixture, preludeTypeInfo)
+    return fromComplicated(node, [": ", typeInstanceSnippet])
 }
 
 function resolvePreStatementGenerator(generator: IndependentCodeGeneratorFunc, state: GeneratorState, fixture: GeneratorFixture): readonly GeneratedSnippets[] {
--- src/test/ts-compiler/valid-program/type/related-function-types/output.ts
+++ src/test/ts-compiler/valid-program/type/related-function-types/output.ts
@@ -1,28 +1,27 @@
 // TODO: The def simple type call generator isn't working as desired..
 const A2 = "himom"
 
-// TODO: None of these classes are getting generated at present.
-class _18 { f() {
+class _T_TypePackage { f() {
   return undefined as unknown as typeof A
 } }
-class _19<_T_Param extends ReturnType<_18['f']>> { f() {
+class _U_TypePackage<_T_Param extends ReturnType<_T_TypePackage["f"]>> { f() {
   const T = undefined as unknown as _T_Param
   const U0 = undefined as unknown as typeof T | number
   return undefined as unknown as typeof U0
 } }
-class _20<_T_Param extends ReturnType<_18['f']>, _U_Param extends ReturnType<_19<_T_Param>['f']>> { f() {
+class _2_TypePackage<_T_Param extends ReturnType<_T_TypePackage["f"]>, _U_Param extends ReturnType<_U_TypePackage<_T_Param>["f"]>> { f() {
   const T = undefined as unknown as _T_Param
   const U0 = undefined as unknown as typeof T | number
   const U = undefined as unknown as _U_Param
   return undefined as unknown as typeof U
 } }
-class _21<_T_Param extends ReturnType<_18['f']>, _U_Param extends ReturnType<_19<_T_Param>['f']>> { f() {
+class _p_TypePackage<_T_Param extends ReturnType<_T_TypePackage["f"]>, _U_Param extends ReturnType<_U_TypePackage<_T_Param>["f"]>> { f() {
   const T = undefined as unknown as _T_Param
   const U0 = undefined as unknown as typeof T | number
   const U = undefined as unknown as _U_Param
   return undefined as unknown as typeof U
 } }
-const funcWithRelatedTypes = <_T_Param extends ReturnType<_18['f']>, _U_Param extends ReturnType<_19<_T_Param>['f']>>(p: ReturnType<_21<_T_Param, _U_Param>['f']>): ReturnType<_20<_T_Param, _U_Param>['f']> => {
+const funcWithRelatedTypes = <_T_Param extends ReturnType<_T_TypePackage["f"]>, _U_Param extends ReturnType<_U_TypePackage<_T_Param>["f"]>>(p: ReturnType<_p_TypePackage<_T_Param, _U_Param>["f"]>): ReturnType<_2_TypePackage<_T_Param, _U_Param>["f"]> => {
   const T = undefined as unknown as _T_Param
   const U0 = undefined as unknown as typeof T | number
   const U = undefined as unknown as _U_Param
