ffc907ce0bf3f509706fb4a1d8664544e9997a5a
Grant Dennison <grant.dennison@gmail.com>
2023-10-05 23:08:09 -0400

    Expose return metadata from interpret functions

--- src/interpreter/statement.ts
+++ src/interpreter/statement.ts
@@ -4,63 +4,96 @@ import { InterpreterErrorWithContext, makeInterpreterError } from "./interpreter
 import { identifierRegex } from "./patterns"
 import { Atom, isAtomLiterally, isSimpleAtom, ThyBlockContext } from "./types"
 
-export function interpretThyStatement(context: ThyBlockContext, parts: readonly Atom[]): void | PromiseLike<void> {
+export function parseThyStatement(parts: readonly Atom[]) {
   const mutableParts = [...parts]
   const firstPart = mutableParts.shift()
-  const isExport = isAtomLiterally(firstPart, "export")
-  const isPrivate = isAtomLiterally(firstPart, "private")
-  const variableNamePart = (isExport || isPrivate) ? mutableParts.shift() : firstPart
+  const varModifierPart = (isAtomLiterally(firstPart, "export") || isAtomLiterally(firstPart, "private")) ? firstPart : undefined
+  const variableNamePart = !!varModifierPart ? mutableParts.shift() : firstPart
   const [assignKeywordPart, ...callParts] = mutableParts
 
   if (isSimpleAtom(variableNamePart) && isSimpleAtom(assignKeywordPart) && ["is", "be", "to"].includes(assignKeywordPart.text)) {
-    const definedVariableNamePart = variableNamePart
-    const variableName = variableNamePart.text
-    const assignKeyword = assignKeywordPart.text
-    if (isAtomLiterally(callParts[0], "await")) {
-      assertAwaitCallParts(callParts)
-      return doAwaitStuff(callParts[0], callParts[1]).then(handleReturnedValue)
+    return {
+      assignment: {
+        varModifierPart: varModifierPart,
+        varPart: variableNamePart,
+        assignPart: assignKeywordPart,
+      },
+      call: {
+        parts: callParts,
+      },
     }
+  }
+  return {
+    call: {
+      parts: parts,
+    },
+  }
+}
+
+export function interpretThyStatement(context: ThyBlockContext, parts: readonly Atom[]): void | PromiseLike<void> {
+  const parsed = parseThyStatement(parts)
+  const callParts = parsed.call.parts
+
+  const handleReturnedValue = getReturnHandler()
+
+  if (isAtomLiterally(callParts[0], "await")) {
+    assertAwaitCallParts(callParts)
+    return doAwaitStuff(callParts[0], callParts[1]).then(handleReturnedValue)
+  }
+
+  handleReturnedValue(interpretThyCall(context, callParts))
+  return
+
+  function getReturnHandler() {
+    if (parsed.assignment) {
+      const varModifierPart = parsed.assignment.varModifierPart
+      const definedVariableNamePart = parsed.assignment.varPart
+      const variableName = parsed.assignment.varPart.text
+      const assignKeyword = parsed.assignment.assignPart.text
+      
+      return function handleReturnedValueAssign(newValue: unknown) {
+        context.thatValue = undefined
+        context.beforeThatValue = undefined
   
-    handleReturnedValue(interpretThyCall(context, callParts))
-    return
-
-    function handleReturnedValue(newValue: unknown) {
-      context.thatValue = undefined
-      context.beforeThatValue = undefined
-
-      if (!identifierRegex.test(variableName)) {
-        throw makeInterpreterError(definedVariableNamePart, `${variableName} is not a valid identifier. Variable names should begin with a lower-case letter and only contain letters and numbers.`)
-      }
-
-      if (assignKeyword !== "to") {
-        if (isExport) {
-          context.exportedVariables.push(variableName)
-        } else if (!isPrivate) {
-          context.bareVariables.push(variableName)
+        if (!identifierRegex.test(variableName)) {
+          throw makeInterpreterError(definedVariableNamePart, `${variableName} is not a valid identifier. Variable names should begin with a lower-case letter and only contain letters and numbers.`)
         }
-      }
-
-      if (variableName in context.implicitArguments) {
-        throw makeInterpreterError(definedVariableNamePart, `${variableName} is an implicit argument and cannot be overwritten`)
-      }
-      if (variableName in context.closure && ["is", "be"].includes(assignKeyword)) {
-        throw makeInterpreterError(definedVariableNamePart, `${variableName} cannot be shadowed. Since it is declared in an upper scope, it cannot be redefined.`)
-      }
-      if (context.variableIsImmutable[variableName] || context.closureVariableIsImmutable[variableName]) {
-        throw makeInterpreterError(definedVariableNamePart, `${variableName} is immutable and cannot be reassigned. Did you mean to use \`be\` instead of \`is\` at its definition?`)
-      }
-      if (variableName in context.variablesInBlock && assignKeyword !== "to") {
-        throw makeInterpreterError(definedVariableNamePart, `${variableName} is already defined. Did you mean to use \`to\` instead of \`${assignKeyword}\`?`)
-      }
-      if (variableName in context.closure) {
-        context.closure[variableName] = newValue
+  
+        if (assignKeyword !== "to") {
+          if (isAtomLiterally(varModifierPart, "export")) {
+            context.exportedVariables.push(variableName)
+          } else if (!isAtomLiterally(varModifierPart, "private")) {
+            context.bareVariables.push(variableName)
+          }
+        }
+  
+        if (variableName in context.implicitArguments) {
+          throw makeInterpreterError(definedVariableNamePart, `${variableName} is an implicit argument and cannot be overwritten`)
+        }
+        if (variableName in context.closure && ["is", "be"].includes(assignKeyword)) {
+          throw makeInterpreterError(definedVariableNamePart, `${variableName} cannot be shadowed. Since it is declared in an upper scope, it cannot be redefined.`)
+        }
+        if (context.variableIsImmutable[variableName] || context.closureVariableIsImmutable[variableName]) {
+          throw makeInterpreterError(definedVariableNamePart, `${variableName} is immutable and cannot be reassigned. Did you mean to use \`be\` instead of \`is\` at its definition?`)
+        }
+        if (variableName in context.variablesInBlock && assignKeyword !== "to") {
+          throw makeInterpreterError(definedVariableNamePart, `${variableName} is already defined. Did you mean to use \`to\` instead of \`${assignKeyword}\`?`)
+        }
+        if (variableName in context.closure) {
+          context.closure[variableName] = newValue
+          return
+        }
+        if (assignKeyword === "is") {
+          context.variableIsImmutable[variableName] = true
+        }
+        context.variablesInBlock[variableName] = newValue
         return
       }
-      if (assignKeyword === "is") {
-        context.variableIsImmutable[variableName] = true
-      }
-      context.variablesInBlock[variableName] = newValue
-      return
+    }
+
+    return function handleReturnedValueNoAssign(newValue: unknown) {
+      context.beforeThatValue = context.thatValue
+      context.thatValue = newValue
     }
   }
 
@@ -78,18 +111,6 @@ export function interpretThyStatement(context: ThyBlockContext, parts: readonly 
       }
     })()
   }
-
-  if (isAtomLiterally(parts[0], "await")) {
-    assertAwaitCallParts(parts)
-    return doAwaitStuff(parts[0], parts[1]).then((result) => {
-      context.beforeThatValue = context.thatValue
-      context.thatValue = result
-    })
-  }
-
-  const result = interpretThyCall(context, parts)
-  context.beforeThatValue = context.thatValue
-  context.thatValue = result
 }
 
 function assertAwaitCallParts(callParts: readonly Atom[]) {
