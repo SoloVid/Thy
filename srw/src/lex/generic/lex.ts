92bc0e6cd92f33539b0d8947a1118d8178da48f0
Grant Dennison <grant.dennison@gmail.com>
2026-09-11 22:37:31 -0400

    Try simplify lex() further

--- srw/src/lex/generic/lex.ts
+++ srw/src/lex/generic/lex.ts
@@ -1,4 +1,3 @@
-import { munchTokensWithGaps } from "./gap-handler.ts"
 import type { Muncher } from "./munch/muncher.ts"
 import type { LexResult } from "./result.ts"
 import type { LexState } from "./state.ts"
@@ -19,13 +18,67 @@ function collectTokens<TokenKind extends string>(
   muncher: Muncher<TokenKind>,
   state: LexState,
 ): LexResult<TokenKind> {
-  return munchTokensWithGaps(state, () => {
-    const tokens: Token<TokenKind>[] = []
-    let nextToken = muncher(state)
-    while (nextToken) {
-      tokens.push(nextToken)
-      nextToken = muncher(state)
-    }
-    return tokens
-  })
+  const tokens: Token<TokenKind | null>[] = []
+  const tokenProcessor = makeTokenProcessor(state, tokens)
+
+  while (state.offset < state.text.length) {
+    const nextToken = muncher(state)
+    tokenProcessor.processNextToken(nextToken)
+  }
+
+  tokenProcessor.finalize()
+
+  return tokens
+}
+
+function makeTokenProcessor<TokenKind extends string>(
+  state: LexState,
+  tokens: Pick<Token<TokenKind | null>[], "push">,
+) {
+  const tokenGapHandler = makeTokenGapHandler(state)
+
+  return {
+    processNextToken(nextToken: Token<TokenKind> | null) {
+      if (nextToken) {
+        tokenGapHandler.appendPrecedingGapToken(tokens)
+        tokens.push(nextToken)
+      } else if (state.offset < state.text.length) {
+        tokenGapHandler.updateTracking()
+        state.offset += 1
+      }
+    },
+    finalize() {
+      tokenGapHandler.appendFinalGapToken(tokens)
+    },
+  }
+}
+
+function makeTokenGapHandler<TokenKind extends string>(
+  state: LexState,
+) {
+  /** The in-process gap token we're building. */
+  let gapToken: Token<null> | null = null
+  return {
+    appendPrecedingGapToken(tokens: Pick<Token<TokenKind | null>[], "push">) {
+      if (gapToken) {
+        tokens.push(gapToken)
+        gapToken = null
+      }
+    },
+    updateTracking() {
+      if (!gapToken) {
+        gapToken = {
+          kind: null,
+          offset: state.offset,
+          length: 0,
+        }
+      }
+      gapToken.length += 1
+    },
+    appendFinalGapToken(tokens: Pick<Token<TokenKind | null>[], "push">) {
+      if (gapToken) {
+        tokens.push(gapToken)
+      }
+    },
+  }
 }
